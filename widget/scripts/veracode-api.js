d6bd4763f9086d1d81fdd41344f11fbe0b1b3b6b
David Longman <apps@d9n.com>
2016-11-22 12:56:27 +0000

    Updated description Attempted to add a loading UI for widget but this does not render in VSTS

--- widget/scripts/veracode-api.js
+++ widget/scripts/veracode-api.js
@@ -1,5 +1,35 @@
 ﻿"use strict";
 
+// updates the widget interface to show the widget is loading otherwise it looks like it has hung
+function updateWidgetProgressIndicator(widgetId, stopProgress, message){
+    try {
+        if (widgetId.indexOf("#") != 0) {
+            widgetId = "#" + widgetId;
+        }
+        var $widget = $(widgetId);
+        
+        if ($widget !== undefined) {
+            // update the progress indicator
+            if (stopProgress) {
+                $(widgetId + " .veracode-widget-loading").hide();
+                $(widgetId + " .veracode-widget-content").show();
+            }
+            else {
+                $(widgetId + " .veracode-widget-loading").show();
+                $(widgetId + " .veracode-widget-content").hide();
+            }
+
+            if (message !== undefined && stopProgress !== true) {
+                $(widgetId + " #loading-text").html = message;
+            }
+        }
+    }
+    catch (e) {
+        console.log("Unable to updateWidgetProgressIndicator");
+        console.log(e);
+    }
+}
+
 // Takes the scan report data from the Veracode API Wrapper and updates the given widget id
 function populateVeracodeScanWidget(data, widgetId) {
 
@@ -25,8 +55,8 @@ function populateVeracodeScanWidget(data, widgetId) {
     }
     
     // update the scan date
-    $(widgetId + " .scan-date").html(moment(data.ScanDate).fromNow());
-    $(widgetId + " .scan-date").attr("title", "Last Scanned on " + data.ScanDate);
+    $(widgetId + " #scan-date").html(moment(data.ScanDate).fromNow());
+    $(widgetId + " #scan-date").attr("title", "Last Scanned on " + data.ScanDate);
 
     if (data.Status === "Pass") {
         $(widgetId).css("background-color", "rgb(51, 153, 51)"); // green
@@ -40,6 +70,8 @@ function populateVeracodeScanWidget(data, widgetId) {
         $(widgetId).css("background-color", "rgb(0, 156, 204)"); // blue
         $(widgetId + " .veracode-shield").addClass("policyDidPassWithWarnings");
     }
+
+    // rgb(170, 1, 178) // pink
 
     // remove the veracode shield if the number of vulnerabilities is more than
     // 99 otherwise the shield will not fit in the 160px width
@@ -61,6 +93,9 @@ function retrieveVeracodeScanDataForWidget(scanId, widgetId, applicationId, user
             widgetId = "#" + widgetId;
         }
 
+        // update the loading UI
+        updateWidgetProgressIndicator(widgetId, false, "Retrieving data...");
+
         $.ajax({
             method: "GET",
             url: "https://headforwards-axappp.azurewebsites.net/api/" + applicationId + "/scans/" + scanId,
@@ -75,6 +110,8 @@ function retrieveVeracodeScanDataForWidget(scanId, widgetId, applicationId, user
             },
             success: function (data, status, jqXHR) {
                 if (jqXHR.status === 200) {
+                    
+                    updateWidgetProgressIndicator(widgetId, false, "Populating widget...");
 
                     populateVeracodeScanWidget(data, widgetId);
 
@@ -84,11 +121,12 @@ function retrieveVeracodeScanDataForWidget(scanId, widgetId, applicationId, user
                 }
             },
             error: function (jqXHR, status, error) {
-                console.log("Error: " + error);
+                updateWidgetProgressIndicator(widgetId, false, "Error retrieving data...");
                 console.log(jqXHR);
             },
             complete: function () {
-                // do nothing
+                // remove the loading interface
+                updateWidgetProgressIndicator(widgetId, true);
             }
         });
     }
